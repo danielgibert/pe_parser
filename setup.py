af5a6d06b6ff33811d942aa9119544b8b6771e62
Daniel Gibert <daniel.gibertlla@gmail.com>
2022-06-16 12:26:09 +0200

    Added readme to pypi package

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 requires = ['nltk', 'mahotas', 'matplotlib']
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='pe_parser',
-      version='0.1',
+      version='0.1.1',
       description='PE parser implementation',
       url='https://github.com/danielgibert/pe_parser',
       author='Daniel Gibert',
@@ -13,5 +18,7 @@ setup(name='pe_parser',
       install_requires=requires,
       zip_safe=False,
       install_package_data=True,
-      python_requires='>=3.6.9'
+      python_requires='>=3.6.9',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       )
